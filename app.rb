1681beaadd4b9cb415b9b6e02a9f0eb14e6014cd
Ella Zhang <eyanchizhang@gmail.com>
2018-07-17 13:36:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "It was so awesome to see Karlie yesterday! She put a lot of our pictures and videos on her Instagram!lol"
+    "It was so awesome to see Karlie yesterday! She put a lot of our pictures and videos on her Instagram! I love her so much! She is so nice!"
   end
 
 end
