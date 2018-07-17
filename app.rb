d6fdbc201077a5a5f7f462f2a71578a967c685ec
Ella Zhang <eyanchizhang@gmail.com>
2018-07-17 13:43:53 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,8 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/Youtube' do 
+  get '/ ' do 
     "It was so awesome to see Karlie yesterday! She put a lot of our pictures and videos on her Instagram! I love her so much! She is so nice!"
   end
+
+get '/sherbet'do
+  "Sherbet Sherbet"
+end
 
 end
